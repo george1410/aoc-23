9c6b02209f25b2cc9142097198fee5f2236006a2
George McCarron <george.mccarron@orbitalwitness.com>
2023-12-04 09:03:13 +0000

    day 4 part 2

--- 04.py
+++ 04.py
@@ -1,14 +1,20 @@
+from collections import defaultdict
 import re
 
 with open("./input/4") as file:
     lines = [line.rstrip() for line in file]
 
 points = []
-for line in lines:
+counts = defaultdict(int)
+for index, line in enumerate(lines):
+    counts[index + 1] += 1
     cleaned = line.split(": ")[1]
     a_list, b_list = [re.split(r"\s+", x.strip()) for x in cleaned.split(" | ")]
     intersection = [x for x in a_list if x in b_list]
     if len(intersection) > 0:
         points.append(pow(2, len(intersection) - 1))
+        for i in range(len(intersection)):
+            counts[i + index + 2] += 1 * counts[index + 1]
 
 print("Part 1: ", sum(points))
+print("Part 2: ", sum(counts.values()))
