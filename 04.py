7bf76debe86c4852e202e46e5f661200e4e1da93
George McCarron <george.mccarron@orbitalwitness.com>
2023-12-04 15:54:59 +0000

    i learnt something

--- 04.py
+++ 04.py
@@ -1,5 +1,4 @@
 from collections import defaultdict
-import re
 
 with open("./input/4") as file:
     lines = [line.rstrip() for line in file]
@@ -9,7 +8,7 @@ counts = defaultdict(int)
 for index, line in enumerate(lines):
     counts[index + 1] += 1
     cleaned = line.split(": ")[1]
-    a_list, b_list = [re.split(r"\s+", x.strip()) for x in cleaned.split(" | ")]
+    a_list, b_list = [x.split() for x in cleaned.split(" | ")]
     intersection = [x for x in a_list if x in b_list]
     if len(intersection) > 0:
         points.append(pow(2, len(intersection) - 1))
